fe344dd072b3f72fb824d5c1d04d75932aa0c6ca
vandanchauhan <vandan.s.chauhan@gmail.com>
2022-07-13 09:38:18 +0530

    Video section + polish changes for other sections

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -21,6 +21,7 @@ import playstore from "../assests/playstore.png"
 // import ReactPlayer from 'react-player'
 // import "bootstrap/dist/css/bootstrap.css"
 import rightArrow from "../assests/rightArrow.png"
+import VideoCarousel from './VideoCarousel.js';
 
 const Homepage = () => {
 
@@ -50,7 +51,7 @@ const Homepage = () => {
         <div className="bg-gradient-to-b from-[#030701] to-[#113402] m-0 p-0  w-auto">
 
             {/* Electripe Icon */}
-            <div className='flex w-40 md:w-50 justify-start mt-8 ml-8'>
+            <div className='flex w-40 md:w-50 justify-start mt-8 ml-6'>
                 <div className=''><img src={logo} /> </div>
             </div>
 
@@ -58,7 +59,7 @@ const Homepage = () => {
             <div className='bg-gradient-to-b from-[#030701] to-[#113402] flex md:justify-between justify- flex-col justify-center items-center pt-8 pl-5 pr-5 md:pr-20 md:pl-20'>
 
                 <div className='flex md:flex-row flex-col items-center justify-between w-100 md:pb-20'>
-                    <div className='text-white px-2 md:pt-12'>
+                    <div className='text-white w-full md:pt-12'>
                         <p className='text-3xl md:text-4xl lg:text-5xl leading-10 font-bold font-sans'> Leading charging </p> <p className='text-3xl md:text-4xl lg:text-5xl leading-10 font-bold font-sans pb-2'>infra provider.</p>
                         <p className='text-base sm:text-lg leading-6 font-light  opacity-60 pt-2'> Say goodbye to days without charging </p> <p className='text-base sm:text-lg leading-6 font-light opacity-60'>stations near you.</p>
                     </div>
@@ -111,7 +112,7 @@ const Homepage = () => {
                     </div>
 
 
-                    <a className='flex justify-center items-center m-5 bg-[#00A878] p-1 py-3'>
+                    <a className='flex justify-center items-center m-4 bg-[#00A878] p-1 py-3'>
                         <p className='text-white text-1xl md:text-3xl font-semibold'>Find Nearby stations</p>
                         <img className='w-5 resize mx-2' src={rightArrow} />
                     </a>
@@ -132,7 +133,7 @@ const Homepage = () => {
 
 
                 <div className=' bg-black-500'><img src={phone} /></div>
-                <div className='bg-black-200 w-2/3 md:w-1/3 py-2 border-2 border-gray-800 mb-4'>
+                <div className='bg-black-200 w-full md:w-1/3 py-2 border-2 border-gray-800 mb-4'>
                     <a className='flex flex-row justify-center items-center'>
                         <img className='w-12 mr-2' src={playstore} />
                         <p className='text-white font-bold text-1xl md:text-2xl'>Download The App</p>
@@ -141,10 +142,10 @@ const Homepage = () => {
             </div>
 
             {/* A strong network of companies supporting us */}
-            <div className='bg-gradient-to-b from-[#030701] to-[#113402] pl-5 pr-5 md:pr-20 md:pl-20 pb-5'>
+            <div className='bg-gradient-to-b from-[#030701] to-[#0B2101] pl-5 pr-5 pt-4 md:pr-20 md:pl-20 pb-5 '>
                 <div className='flex flex-col bg-gradient-to-b from-[#113402] to-[#030701] pl-4 pr-4 md:pr-20 md:pl-20 pb-5  justify-center items-center' >
-                    <div className=' mt-5'>
-                        <h1 className='text-white text-xl font-medium pl-2 pt-2'>
+                    <div className=' mt-4'>
+                        <h1 className='text-white text-2xl font-medium mb-2'>
                             <span className='text-[#00A878]'>A strong network</span> of companies supporting us</h1>
                     </div>
                     <div className='flex m-1'>
@@ -186,7 +187,7 @@ const Homepage = () => {
                         </div>
                     </div>
 
-                    <a className='flex justify-center items-center m-5 bg-[#00A878] p-2 py-3 w-full'>
+                    <a className='flex justify-center items-center bg-[#00A878] p-2 py-3 mt-4 mx-4 w-full'>
                         <p className='text-white text-1xl md:text-4xl font-semibold'>Become Partner</p>
                         <img className='w-5 resize mx-2' src={rightArrow} />
                     </a>
@@ -195,106 +196,84 @@ const Homepage = () => {
 
             {/* And thousands of Indians who believe in us */}
 
-            <div className='bg-gradient-to-b px-4 from-[#030701] to-[#0B2101]'>
+            <div className='bg-gradient-to-b px-4 from-[#0B2101] to-[#030701]'>
 
 
-                {/* <div div className='flex flex-col mt-0 m-5 p-4' >
-                    <div className='m-2'><p className='text-white font-semibold text-3xl'>
-                        And thousands of Indians who believe in us</p>
+                <div div className='flex flex-col' >
+                    <div className='my-4'><p className='text-white font-semibold text-2xl'>
+                        <span className='text-[#00A878]'>And thousands of Indians </span> <br/>
+                        who believe in us</p>
                     </div>
-                    <div className='flex justify-center'>
-                        <div>
-                            <Carousel>
-                                {videoProperties.map((videoObj) => {
-                                    return (
-                                        <Carousel.Item key={videoObj.id}>
-                                            <ReactPlayer
-                                                url={videoObj.src}
-                                                // width="100%"
-                                                pip={true}
-                                                controls={true}
-                                                playing={true}
-
-                                            />
-                                            <Carousel.Caption>
-                                                <h3>{videoObj.title}</h3>
-                                                <p>{videoObj.credit}</p>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
-                                    )
-                                })}
-                            </Carousel>
-                        </div>
+                    <div>
+                            <VideoCarousel/>
                     </div>
                     <div className='flex justify-start p-2'>
                         <div className=''><p className='text-white text-1xl'>Abhishek <span className='text-gray-400'>E-rikshaw Driver</span></p></div>
                     </div>
-                </div> */}
+                </div>
 
 
-                <div className='flex flex-col pl-5 pr-5 md:pr-20 md:pl-20 w-auto'>
+                <div className='flex flex-col md:pr-20 md:pl-20 w-auto'>
                     <div>
-                        <p className='text-white text-3xl md:text-4xl font-bold pt-5'> In News</p>
+                        <p className='text-white text-2xl md:text-4xl font-bold pt-5'> In News</p>
                     </div>
 
-                    <div className='w-full h-40 md:h-96'>
-                        <div className='flex mt-4 snap-x mx-auto w-full  h-40 md:h-80 overflow-scroll'>
-
-                            <div className='w-full h-40 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 snap-start'>
+                    <div className='w-full h-28 md:h-96'>
+                        <div className='flex mt-4 mr-10 snap-x mx-auto w-full h-28 md:h-80 overflow-scroll'>
+                            <div className='w-72 h-24 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 mr-3 snap-start bg-gradient-to-b from-[#113402] to-[#0C2302]'>
                                 <p className='text-white text-base font-semibold font-inter normal'><span className='text-[#00A878]'>ElectricPe</span> raises 220M in seed fund</p>
-                                <img className='w-5 ml-1 mt-1' src={rightArrow} />
+                                <img className='w-5 ml-1 -mt-4' src={rightArrow} />
                             </div>
 
-                            <div className='w-full h-40 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 snap-start'>
+                            <div className='w-72 h-24 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 mr-3 snap-start bg-gradient-to-b from-[#113402] to-[#0C2302]'>
                                 <p className='text-white text-base font-semibold font-inter normal'><span className='text-[#00A878]'>ElectricPe</span> raises 220M in seed fund</p>
-                                <img className='w-5 ml-1 mt-1' src={rightArrow} />
+                                <img className='w-5 ml-1 -mt-4' src={rightArrow} />
                             </div>
-
-                            <div className='w-full h-40 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 snap-start'>
+                            <div className='w-72 h-24 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 mr-3 snap-start bg-gradient-to-b from-[#113402] to-[#0C2302]'>
                                 <p className='text-white text-base font-semibold font-inter normal'><span className='text-[#00A878]'>ElectricPe</span> raises 220M in seed fund</p>
-                                <img className='w-5 ml-1 mt-1' src={rightArrow} />
+                                <img className='w-5 ml-1 -mt-4' src={rightArrow} />
                             </div>
-
-                            <div className='w-full h-40 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 snap-start'>
+                            <div className='w-72 h-24 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 mr-3 snap-start bg-gradient-to-b from-[#113402] to-[#0C2302]'>
                                 <p className='text-white text-base font-semibold font-inter normal'><span className='text-[#00A878]'>ElectricPe</span> raises 220M in seed fund</p>
-                                <img className='w-5 ml-1 mt-1' src={rightArrow} />
+                                <img className='w-5 ml-1 -mt-4' src={rightArrow} />
                             </div>
-
-                            <div className='w-full h-40 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 snap-start'>
+                            <div className='w-72 h-24 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 mr-3 snap-start bg-gradient-to-b from-[#113402] to-[#0C2302]'>
                                 <p className='text-white text-base font-semibold font-inter normal'><span className='text-[#00A878]'>ElectricPe</span> raises 220M in seed fund</p>
-                                <img className='w-5 ml-1 mt-1' src={rightArrow} />
+                                <img className='w-5 ml-1 -mt-4' src={rightArrow} />
                             </div>
-                        </div></div>
+                            <div className='w-72 h-24 md:h-80 px-3 py-4 flex items-center flex-shrink-0 justify-center bg-[#090D08] m-2 mr-3 snap-start bg-gradient-to-b from-[#113402] to-[#0C2302]'>
+                                <p className='text-white text-base font-semibold font-inter normal'><span className='text-[#00A878]'>ElectricPe</span> raises 220M in seed fund</p>
+                                <img className='w-5 ml-1 -mt-4' src={rightArrow} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
 
                 {/* Learn From Our Blogs */}
-                <div div className='flex flex-col pl-5 pr-5 md:pr-20 md:pl-20 w-auto'>
+                <div div className='flex flex-col md:pr-20 md:pl-20 w-auto'>
                     <div>
-                        <p className='text-white text-3xl md:text-4xl font-bold pt-5'>Learn from our blogs</p>
+                        <p className='text-white text-2xl md:text-4xl font-bold pt-5'>Learn from our blogs</p>
                     </div>
 
                     <div className='flex items-center mt-4 snap-x mx-auto w-80 overflow-scroll'>
-                        <div className='flex flex-col my-6 w-72 flex-shrink-0 snap-start mx-2'>
-                            <div className='h-40 bg-[#383838]'></div>
-                            <div className='bg-[#090D08] px-2 py-1'><p className='text-white text-1xl font-semibold font-inter text-start p-4'>Safety First Station For All</p></div>
+                        <div className='flex flex-col my-1 w-72 flex-shrink-0 snap-start mx-2 bg-gradient-to-b from-[#113302] to-[#0D2402]'>
+                            <div className='h-40 bg-[#ffffff] opacity-10'></div>
+                            <div className='bg-gradient-to-b from-[#113302] to-[#0D2402] py-1'><p className='text-white text-1xl font-semibold font-inter text-start p-3'>Safety First Station For All</p></div>
                         </div>
-
-                        <div className='flex flex-col my-6 w-72 flex-shrink-0 snap-start mx-2'>
-                            <div className='h-40 bg-[#383838]'></div>
-                            <div className='bg-[#090D08] px-2 py-1'><p className='text-white text-1xl font-semibold font-inter text-start p-4'>Safety First Station For All</p></div>
+                        <div className='flex flex-col my-1 w-72 flex-shrink-0 snap-start mx-2 bg-gradient-to-b from-[#113302] to-[#0D2402]'>
+                            <div className='h-40 bg-[#ffffff] opacity-10'></div>
+                            <div className='bg-gradient-to-b from-[#113302] to-[#0D2402] py-1'><p className='text-white text-1xl font-semibold font-inter text-start p-3'>Safety First Station For All</p></div>
                         </div>
-
-                        <div className='flex flex-col my-6 w-72 flex-shrink-0 snap-start mx-2'>
-                            <div className='h-40 bg-[#383838]'></div>
-                            <div className='bg-[#090D08] px-2 py-1'><p className='text-white text-1xl font-semibold font-inter text-start p-4'>Safety First Station For All</p></div>
+                        <div className='flex flex-col my-1 w-72 flex-shrink-0 snap-start mx-2 bg-gradient-to-b from-[#113302] to-[#0D2402]'>
+                            <div className='h-40 bg-[#ffffff] opacity-10'></div>
+                            <div className='bg-gradient-to-b from-[#113302] to-[#0D2402] py-1'><p className='text-white text-1xl font-semibold font-inter text-start p-3'>Safety First Station For All</p></div>
                         </div>
                     </div>
                 </div >
-            </div>
 
-            {/* Footer */}
-            <div div className='p-8 flex flex-col bg-[#090D08]' >
+                            {/* Footer */}
+            <div div className='py-8 flex flex-col bg-[#0000]' >
                 <div className='text-[#666666] pb-2 pt-2'>Our Website</div>
                 <div className='text-[#666666] pb-2 pt-2'>Contact Us</div>
 
@@ -319,6 +298,7 @@ const Homepage = () => {
 
                 <div className='flex text-[#666666] pb-2 pt-4'>Whatsapp Technologies Private Limited</div>
             </div >
+            </div>
 
 
 
